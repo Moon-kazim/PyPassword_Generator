61e2ca4ce37775ec0233fc3afd6a37c4ee272646
MOON-KAZIM <kazim.shaikh0@gmail.com>
2024-12-23 17:06:12 +0530

    updated type1

--- PyPassword_Generator_type2.py
+++ PyPassword_Generator_type2.py
@@ -1,22 +1,34 @@
-#easyVersion
-#Generate the password in sequence. Letters, then symbols, then numbers.
+#hardVersion
+#Generate the password doesn't follow any pattern.
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome To PyPassword Generator")
+#taking input from the user
 k_letters = int(input("How many letters would you like to print in your password? "))
 k_symbols = int(input("How many symbols would you like? "))
 k_numbers = int(input("How many numbers would you like? "))
 
 password = ""
 
-for char in range(k_letters): 
+for char in range(1, k_letters+1):
     password += random.choice(letters)
-for symb in range(0, k_symbols):
+for symb in range(1, k_symbols+1):
     password += random.choice(symbols)
-for numb in range(k_numbers):
+for numb in range(1, k_numbers+1):
     password += random.choice(numbers)
+
+shuffled_password = random.sample(password, len(password))
 print(password)
+# random.shuffle(password)
+print(shuffled_password)
+
+
+
+# new_password = random.shuffle(password)
+
+# print(new_password)
+
+
